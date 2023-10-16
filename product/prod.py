026478f1264e20110917823a73d65baf85cc8c52
Mr-Rajesh24 <86848521+Mr-Rajesh24@users.noreply.github.com>
2023-10-16 11:01:27 +0530

    Create prod.py

---
+++ product/prod.py
@@ -0,0 +1,13 @@
+def calc(num1, num2):
+    product = num1 * num2
+    return product
+
+
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+prod = calc(n1, n2)
+
+print("input python mul", n1, n2)
+
+print(prod)
